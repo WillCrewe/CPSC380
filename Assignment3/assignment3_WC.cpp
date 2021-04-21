0dd9b182223e0d0b607191d7c29676fcbc5a2d1a
WillCrewe <47368039+WillCrewe@users.noreply.github.com>
2021-04-21 15:01:48 -0700

    Update assignment3_WC.cpp

--- Assignment3/assignment3_WC.cpp
+++ Assignment3/assignment3_WC.cpp
@@ -6,6 +6,10 @@
 #include <unistd.h>
 using namespace std;
 
+//Website that helped me figure out the syntax for semaphore https://www.bogotobogo.com/cplusplus/multithreading_pthread.php
+
+#define MAX 1
+
 sem_t sem1;
 sem_t sem2;
 
@@ -20,36 +24,39 @@ void *see(void *arg){
       {
         FredHeight++;
         WilmaHeight--;
-        cout << "See: Fred Height: " << FredHeight << endl;
-        cout << "See: Wilma Height: " << WilmaHeight << endl;
+        cout << "See: Fred Height: " << FredHeight << " Wilma Height: " << WilmaHeight << endl;
         sleep(1);
       }
+      cout << "SWITCHING DIRECTIONS" << endl;
+      sem_post(&sem2);
       count++;
     }
-      sem_post(&sem2);
   }
 
 void *saw(void *arg){
   int count1 = 0;
   while(count1 < 5)
   {
-    double WilmaHeight = 7;
-    double FredHeight = 1;
+    double WilmaHeight = 1;
+    double FredHeight = 7;
     sem_wait(&sem2);
-    while(WilmaHeight > 1)
+    while(WilmaHeight < 7)
     {
-      WilmaHeight -= 1.5;
-      FredHeight += 1.5;
-      cout << "Saw: Fred Height: " << FredHeight << endl;
-      cout << "Saw: Wilma Height: " << WilmaHeight << endl;
+      WilmaHeight += 1.5;
+      FredHeight -= 1.5;
+      cout << "Saw: Fred Height: " << FredHeight << " Wilma Height: " << WilmaHeight << endl;
       sleep(1);
     }
+      cout << "SWITCHING DIRECTIONS" << endl;
+      sem_post(&sem1);
       count1++;
   }
-    sem_post(&sem1);
 }
 
 int main(){
+
+  sem_init(&sem1, 0, MAX);
+  sem_init(&sem2, 0, 0);
 
   pthread_t see_thread;
   pthread_t saw_thread;
@@ -57,8 +64,8 @@ int main(){
   int int_see;
   int int_saw;
 
-  int_see = pthread_create(&see_thread, NULL, &see, NULL);
-  int_saw = pthread_create(&saw_thread, NULL, &saw, NULL);
+  int_see = pthread_create(&see_thread, 0, &see, 0);
+  int_saw = pthread_create(&saw_thread, 0, &saw, 0);
 
   pthread_join(see_thread, NULL);
   pthread_join(saw_thread, NULL);
